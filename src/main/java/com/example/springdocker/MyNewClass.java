af2a00577713c8842933ed10d4a0b2072d4d385e
JontesGitHub <johannes.svensson97@gmail.com>
2021-05-12 13:07:29 +0200

    added 2 methods

--- src/main/java/com/example/springdocker/MyNewClass.java
+++ src/main/java/com/example/springdocker/MyNewClass.java
@@ -3,4 +3,12 @@ package com.example.springdocker;
 public class MyNewClass {
     public String firstname;
     public String lastname;
+
+    public int getAge() {
+        return 20;
+    }
+
+    public String getNameAndAge() {
+        return firstname + " " + lastname + " is: " + getAge() + " old.";
+    }
 }
